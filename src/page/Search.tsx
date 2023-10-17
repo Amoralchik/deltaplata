7d2696d81bd115cd1ca8a0bcc5c240334314b563
Amoralchik <dariysdemonad@gmail.com>
2023-10-17 18:39:02 +0300

    empty page for search

--- src/page/Search.tsx
+++ src/page/Search.tsx
@@ -1,185 +1,59 @@
 import ItemCard from '../components/ItemCard';
-import { Badge, Group, SimpleGrid, Stack } from '@mantine/core';
+import { Badge, Group, SimpleGrid, Stack, Image, Text, Grid, Button, Flex } from '@mantine/core';
 import RightBar from '../components/RightBar';
 import ContentHeader from '../components/ContentHeader';
 import { useParams } from 'react-router-dom';
 import { useState } from 'react';
+import image11 from './../img/image11.png';
+
+interface item {
+  date: string;
+  personName: string;
+  country: string;
+  itemDate: string;
+  address: string;
+  tags: string[];
+  key: number;
+}
 
 export default function Search() {
   const { text } = useParams();
-  const [items, setItems] = useState([
-    {
-      date: '30 May, 2023',
-      personName: 'Nora Frey Draeger',
-      country: 'British',
-      itemDate: 'Setember 1978',
-      address: 'Flat 9, 112 Belgrave Road, London, SW1V 2BL',
-      tags: ['Bank', 'Political party', 'Person of interest', 'Financial crim'],
-      key: 1,
-    },
-    {
-      date: '30 May, 2023',
-      personName: 'Nora Frey Draeger',
-      country: 'British',
-      itemDate: 'Setember 1978',
-      address: 'Flat 9, 112 Belgrave Road, London, SW1V 2BL',
-      tags: ['Bank', 'Political party', 'Person of interest', 'Financial crim'],
-      key: 2,
-    },
-    {
-      date: '30 May, 2023',
-      personName: 'Nora Frey Draeger',
-      country: 'British',
-      itemDate: 'Setember 1978',
-      address: 'Flat 9, 112 Belgrave Road, London, SW1V 2BL',
-      tags: ['Bank', 'Political party', 'Person of interest', 'Financial crim'],
-      key: 3,
-    },
-    {
-      date: '30 May, 2023',
-      personName: 'Nora Frey Draeger',
-      country: 'British',
-      itemDate: 'Setember 1978',
-      address: 'Flat 9, 112 Belgrave Road, London, SW1V 2BL',
-      tags: ['Bank', 'Political party', 'Person of interest', 'Financial crim'],
-      key: 4,
-    },
-    {
-      date: '30 May, 2023',
-      personName: 'Nora Frey Draeger',
-      country: 'British',
-      itemDate: 'Setember 1978',
-      address: 'Flat 9, 112 Belgrave Road, London, SW1V 2BL',
-      tags: ['Bank', 'Political party', 'Person of interest', 'Financial crim'],
-      key: 5,
-    },
-    {
-      date: '30 May, 2023',
-      personName: 'Nora Frey Draeger',
-      country: 'British',
-      itemDate: 'Setember 1978',
-      address: 'Flat 9, 112 Belgrave Road, London, SW1V 2BL',
-      tags: ['Bank', 'Political party', 'Person of interest', 'Financial crim'],
-      key: 6,
-    },
-    {
-      date: '30 May, 2023',
-      personName: 'Nora Frey Draeger',
-      country: 'British',
-      itemDate: 'Setember 1978',
-      address: 'Flat 9, 112 Belgrave Road, London, SW1V 2BL',
-      tags: ['Bank', 'Political party', 'Person of interest', 'Financial crim'],
-      key: 7,
-    },
-    {
-      date: '30 May, 2023',
-      personName: 'Nora Frey Draeger',
-      country: 'British',
-      itemDate: 'Setember 1978',
-      address: 'Flat 9, 112 Belgrave Road, London, SW1V 2BL',
-      tags: ['Bank', 'Political party', 'Person of interest', 'Financial crim'],
-      key: 8,
-    },
-    {
-      date: '30 May, 2023',
-      personName: 'Nora Frey Draeger',
-      country: 'British',
-      itemDate: 'Setember 1978',
-      address: 'Flat 9, 112 Belgrave Road, London, SW1V 2BL',
-      tags: ['Bank', 'Political party', 'Person of interest', 'Financial crim'],
-      key: 9,
-    },
-    {
-      date: '30 May, 2023',
-      personName: 'Nora Frey Draeger',
-      country: 'British',
-      itemDate: 'Setember 1978',
-      address: 'Flat 9, 112 Belgrave Road, London, SW1V 2BL',
-      tags: ['Bank', 'Political party', 'Person of interest', 'Financial crim'],
-      key: 10,
-    },
-    {
-      date: '30 May, 2023',
-      personName: 'Nora Frey Draeger',
-      country: 'British',
-      itemDate: 'Setember 1978',
-      address: 'Flat 9, 112 Belgrave Road, London, SW1V 2BL',
-      tags: ['Bank', 'Political party', 'Person of interest', 'Financial crim'],
-      key: 11,
-    },
-    {
-      date: '30 May, 2023',
-      personName: 'Nora Frey Draeger',
-      country: 'British',
-      itemDate: 'Setember 1978',
-      address: 'Flat 9, 112 Belgrave Road, London, SW1V 2BL',
-      tags: ['Bank', 'Political party', 'Person of interest', 'Financial crim'],
-      key: 12,
-    },
-    {
-      date: '30 May, 2023',
-      personName: 'Nora Frey Draeger',
-      country: 'British',
-      itemDate: 'Setember 1978',
-      address: 'Flat 9, 112 Belgrave Road, London, SW1V 2BL',
-      tags: ['Bank', 'Political party', 'Person of interest', 'Financial crim'],
-      key: 13,
-    },
-    {
-      date: '30 May, 2023',
-      personName: 'Nora Frey Draeger',
-      country: 'British',
-      itemDate: 'Setember 1978',
-      address: 'Flat 9, 112 Belgrave Road, London, SW1V 2BL',
-      tags: ['Bank', 'Political party', 'Person of interest', 'Financial crim'],
-      key: 14,
-    },
-    {
-      date: '30 May, 2023',
-      personName: 'Nora Frey Draeger',
-      country: 'British',
-      itemDate: 'Setember 1978',
-      address: 'Flat 9, 112 Belgrave Road, London, SW1V 2BL',
-      tags: ['Bank', 'Political party', 'Person of interest', 'Financial crim'],
-      key: 15,
-    },
-    {
-      date: '30 May, 2023',
-      personName: 'Nora Frey Draeger',
-      country: 'British',
-      itemDate: 'Setember 1978',
-      address: 'Flat 9, 112 Belgrave Road, London, SW1V 2BL',
-      tags: ['Bank', 'Political party', 'Person of interest', 'Financial crim'],
-      key: 16,
-    },
-    {
-      date: '30 May, 2023',
-      personName: 'Nora Frey Draeger',
-      country: 'British',
-      itemDate: 'Setember 1978',
-      address: 'Flat 9, 112 Belgrave Road, London, SW1V 2BL',
-      tags: ['Bank', 'Political party', 'Person of interest', 'Financial crim'],
-      key: 17,
-    },
-  ]);
+  const [items, setItems] = useState<item[]>([]);
   return (
     <Stack>
       <ContentHeader title={`Result for: “${text}”`} titleChildren={<Badge miw={50}>{items.length}</Badge>} />
-      <Group align='flex-start'>
+      <Group align='center' wrap='nowrap'>
         <RightBar />
-        <SimpleGrid cols={4} pr={4} mah={700} style={{ overflowY: 'auto' }}>
-          {items.map((item) => (
-            <ItemCard
-              key={item.key}
-              date={item.date}
-              personName={item.personName}
-              country={item.country}
-              itemDate={item.itemDate}
-              address={item.address}
-              tags={item.tags}
-            />
-          ))}
-        </SimpleGrid>
+        {items.length ? (
+          <SimpleGrid cols={4} pr={4} mah={700} style={{ overflowY: 'auto' }}>
+            {items.map((item) => (
+              <ItemCard
+                key={item.key}
+                date={item.date}
+                personName={item.personName}
+                country={item.country}
+                itemDate={item.itemDate}
+                address={item.address}
+                tags={item.tags}
+              />
+            ))}
+          </SimpleGrid>
+        ) : (
+          <Flex miw='100%' justify='center'>
+            <Stack align='center' justify='center' miw={550} gap='md'>
+              <Image src={image11} mah={300} w='auto' />
+              <Stack align='center' gap='xs'>
+                <Text size='lg' fw={700}>
+                  Hi there ! I'm your search assistant
+                </Text>
+                <Text size='xl'>Please enter the keyword you want to search for</Text>
+              </Stack>
+              <Button color='#141413' radius='lg'>
+                <Text c='#FFFFFF'>Advanced Search</Text>
+              </Button>
+            </Stack>
+          </Flex>
+        )}
       </Group>
     </Stack>
   );
